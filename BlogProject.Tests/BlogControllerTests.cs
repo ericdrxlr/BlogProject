1036167cf24959ac7e1f20ad36ef25329250ee30
joshuatreeee <joshuatreeee@gmail.com>
2020-02-20 16:10:45 -0500

    added BlogController, BlogView, and BlogControllerTests

---
+++ BlogProject.Tests/BlogControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using BlogProject.Controllers;
+
+namespace BlogProject.Tests
+{
+    public class BlogControllerTests
+    {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new BlogController();
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ BlogProject/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BlogProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
